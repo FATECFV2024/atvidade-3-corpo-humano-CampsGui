97a2d1ccf2d95ca682e9be4f80c345adfc47ca85
Guilherme Alves <guigui21alves@gmail.com>
2024-09-21 14:32:46 -0300

    [feat] Classe main completa

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,12 @@
 package main.java;
 
 public class main {
-  
+  public static void Main(String[] args) {
+      CorpoHumano c1 = new CorpoHumano(70, 0.065f, 1.75f);
+
+      c1.setMassa(2); 
+      System.out.println("Massa: " + c1.getMassa());
+      
+      System.out.println("IMC: " + c1.calcularIMC());
+  }
 }
