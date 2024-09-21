d35560cf44971335b5b99efabe80b2c317fe7ed3
Guilherme Alves <guigui21alves@gmail.com>
2024-09-21 14:42:47 -0300

    [feat] Adição da main ao App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,12 @@
 package main.java;
-import java.util.Scanner;
 
 public class App {
-        //Comentário
+    public static void main (String[] args){
+        CorpoHumano c1 = new CorpoHumano(70, 0.065f, 1.75f);
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
+        c1.setMassa(2); 
+        System.out.println("Massa: " + c1.getMassa());
         
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
-    }
+        System.out.println("IMC: " + c1.calcularIMC());
+}
 }
